91b17851d6ff15f8f16d94334ea4dc680b55f021
Youness Idbakkasse <itsblanklob@gmail.com>
2024-08-11 15:19:59 +0200

    Added support with rem plugin

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,7 +1,13 @@
 import type { Config } from 'tailwindcss'
+import remToPx from 'tailwindcss-rem-to-px';
 
 export default {
   content: ['./*/*.{liquid,json,js}'],
-  theme: {},
-  plugins: []
+  theme: {
+  },
+  plugins: [
+    remToPx({
+      baseFontSize: 16
+    }),
+  ],
 } satisfies Config
